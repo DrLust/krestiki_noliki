b909760554fb8ac8eb76a49c8fbc7fcbc74100d9
Oleg Borodin <olegka_b@list.ru>
2023-12-11 18:45:59 +0300

    c

--- moswarSelenium/character.py
+++ moswarSelenium/character.py
@@ -1,7 +1,7 @@
 class Character():
     def __init__(self):
         self.name = None
-        self.password = '42x5JVH5Sr37ee7'
-        self.email = 'O.BORODIN@LIST.RU'
+        self.password = None
+        self.email = None
         self.hp_full = None
         self.money = None
