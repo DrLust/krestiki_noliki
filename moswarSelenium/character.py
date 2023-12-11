e1877fc47cd5576be4ee4398772351775329a8b6
DrLust <137390706+DrLust@users.noreply.github.com>
2023-12-11 18:49:42 +0300

    Delete moswarSelenium directory

--- moswarSelenium/character.py
+++
@@ -1,7 +0,0 @@
-class Character():
-    def __init__(self):
-        self.name = None
-        self.password = None
-        self.email = None
-        self.hp_full = None
-        self.money = None
--- moswarSelenium/main.py
+++
@@ -1,8 +0,0 @@
-from bot_core import BotCore
-
-if __name__ == '__main__':
-    bot = BotCore()
-    bot.run()
-
-
-
